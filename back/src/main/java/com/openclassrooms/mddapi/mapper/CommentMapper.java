433ea74d58ee6b2796b03bc294432512f0ed9113
Frederic Guyont <you@example.com>
2024-04-05 19:14:43 +0200

    ADD: mapper

---
+++ back/src/main/java/com/openclassrooms/mddapi/mapper/CommentMapper.java
@@ -0,0 +1,46 @@
+package com.openclassrooms.mddapi.mapper;
+
+import com.openclassrooms.mddapi.dto.CommentDto;
+import com.openclassrooms.mddapi.dto.PostDto;
+import com.openclassrooms.mddapi.model.Comment;
+import com.openclassrooms.mddapi.model.Post;
+import com.openclassrooms.mddapi.model.User;
+import com.openclassrooms.mddapi.service.PostService;
+import com.openclassrooms.mddapi.service.SubjectService;
+import com.openclassrooms.mddapi.service.UserService;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Mapper(componentModel = "spring", uses = {PostService.class}, imports = {Arrays.class, Collectors.class, Post.class, User.class, Comment.class, Collections.class, Optional.class})
+public abstract class CommentMapper implements EntityMapper<CommentDto, Comment> {
+
+    @Autowired
+    PostService postService;
+    @Autowired
+    UserService userService;
+
+    @Mappings({
+            @Mapping(source = "text", target = "text"),
+            @Mapping(source = "date", target = "date"),
+            @Mapping(target = "post", expression = "java(this.postService.getById(commentDto.getPostId()))"),
+            @Mapping(target = "user", expression = "java(this.userService.findById(commentDto.getUserId()))"),
+    })
+    public abstract Comment toEntity(CommentDto commentDto);
+
+
+    @Mappings({
+            @Mapping(source = "text", target = "text"),
+            @Mapping(source = "date", target = "date"),
+            @Mapping(source = "comment.post.id", target = "postId"),
+            @Mapping(source = "comment.user.id", target = "userId"),
+    })
+    public abstract CommentDto toDto(Comment comment);
+}
