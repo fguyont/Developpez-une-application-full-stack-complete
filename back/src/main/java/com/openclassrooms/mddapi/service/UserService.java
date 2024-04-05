d59bf9fabf53f2dd9f3600128936bd913857daeb
Frederic Guyont <you@example.com>
2024-04-05 19:14:58 +0200

    ADD: service

---
+++ back/src/main/java/com/openclassrooms/mddapi/service/UserService.java
@@ -0,0 +1,16 @@
+package com.openclassrooms.mddapi.service;
+
+import com.openclassrooms.mddapi.model.User;
+import com.openclassrooms.mddapi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User findById(Long id) {
+        return this.userRepository.findById(id).orElse(null);
+    }
+}
