43d4c27f53e01a4734272d556c27ef1b0bd62faa
Frederic Guyont <you@example.com>
2024-04-05 19:15:08 +0200

    ADD: controller

---
+++ back/src/main/java/com/openclassrooms/mddapi/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.openclassrooms.mddapi.controller;
+
+import com.openclassrooms.mddapi.mapper.UserMapper;
+import com.openclassrooms.mddapi.model.User;
+import com.openclassrooms.mddapi.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+    @Autowired
+    private UserMapper userMapper;
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getById(@PathVariable("id") String id) {
+        try {
+            User user = this.userService.findById(Long.valueOf(id));
+
+            if (user == null) {
+                return ResponseEntity.notFound().build();
+            }
+
+            return ResponseEntity.ok().body(this.userMapper.toDto(user));
+        } catch (NumberFormatException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+}
