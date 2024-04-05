0e81d4dc9ce96f03ba089971fcd5b2c9ed1b286e
Frederic Guyont <you@example.com>
2024-04-05 19:12:28 +0200

    ADD: models

---
+++ back/src/main/java/com/openclassrooms/mddapi/model/Comment.java
@@ -0,0 +1,40 @@
+package com.openclassrooms.mddapi.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+@Entity
+@Table(name = "comments")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 2000)
+    @Column(name = "text")
+    private String text;
+
+    @CreatedDate
+    @Column(name = "date", updatable = false)
+    private Date date;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id", referencedColumnName = "id")
+    private Post post;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+}
