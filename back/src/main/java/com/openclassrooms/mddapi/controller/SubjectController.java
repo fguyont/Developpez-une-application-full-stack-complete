b6d907436a72fe3d2191867238d56bb8d25e0521
Frederic Guyont <you@example.com>
2024-04-10 08:54:39 +0200

    ADD: getById

--- back/src/main/java/com/openclassrooms/mddapi/controller/SubjectController.java
+++ back/src/main/java/com/openclassrooms/mddapi/controller/SubjectController.java
@@ -2,10 +2,12 @@ package com.openclassrooms.mddapi.controller;
 
 import com.openclassrooms.mddapi.mapper.SubjectMapper;
 import com.openclassrooms.mddapi.model.Subject;
+import com.openclassrooms.mddapi.model.Subject;
 import com.openclassrooms.mddapi.service.SubjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +27,20 @@ public class SubjectController {
     public ResponseEntity<?> getAll() {
         List<Subject> subjects = this.subjectService.getAll();
         return ResponseEntity.ok().body(this.subjectMapper.toDto(subjects));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getById(@PathVariable("id") String id) {
+        try {
+            Subject subject = this.subjectService.getById(Long.valueOf(id));
+
+            if (subject == null) {
+                return ResponseEntity.notFound().build();
+            }
+
+            return ResponseEntity.ok().body(this.subjectMapper.toDto(subject));
+        } catch (NumberFormatException e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 }
