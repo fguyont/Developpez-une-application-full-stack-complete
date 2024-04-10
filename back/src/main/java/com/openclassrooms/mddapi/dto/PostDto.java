6f757f390c3014ff6aa441518769307a2a361e8c
Frederic Guyont <you@example.com>
2024-04-10 08:57:19 +0200

    ADD: todo about user data to get in the post

--- back/src/main/java/com/openclassrooms/mddapi/dto/PostDto.java
+++ back/src/main/java/com/openclassrooms/mddapi/dto/PostDto.java
@@ -1,7 +1,5 @@
 package com.openclassrooms.mddapi.dto;
 
-import com.openclassrooms.mddapi.model.Subject;
-import com.openclassrooms.mddapi.model.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +7,6 @@ import lombok.NoArgsConstructor;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -28,6 +25,7 @@ public class PostDto {
     @Size(max = 5000)
     private String text;
 
+    //TODO: I might need more information about the user in the post like his name. I need an another solution (if I have, PostMapper to fix).
     private Long userId;
 
     private Long subjectId;
