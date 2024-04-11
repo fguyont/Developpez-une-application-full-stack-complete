7b39ce83bae10253bffdacf5331ddb29aec931a9
Frederic Guyont <you@example.com>
2024-04-11 10:52:22 +0200

    ADD: update user

--- back/src/main/java/com/openclassrooms/mddapi/service/UserService.java
+++ back/src/main/java/com/openclassrooms/mddapi/service/UserService.java
@@ -3,6 +3,7 @@ package com.openclassrooms.mddapi.service;
 import com.openclassrooms.mddapi.model.User;
 import com.openclassrooms.mddapi.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,7 +11,24 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     public User getById(Long id) {
         return this.userRepository.findById(id).orElse(null);
+    }
+
+    public User update(User user, String email, String name, String password) {
+
+        if (user != null) {
+            user.setEmail(email);
+            user.setName(name);
+            user.setPassword(passwordEncoder.encode(password));
+        }
+
+        return this.userRepository.save(user);
     }
 }
