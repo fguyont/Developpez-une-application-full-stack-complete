3e7d67a98174ecd3a495d6a064e4ea5b6943b800
Frederic Guyont <you@example.com>
2024-04-17 16:12:34 +0200

    ADD: feed posts, all subjects and navbar

---
+++ front/src/app/pages/subject/subject.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Subject } from 'src/app/models/subject';
+import { SubjectService } from 'src/app/services/subject.service';
+
+@Component({
+  selector: 'app-subject',
+  templateUrl: './subject.component.html',
+  styleUrls: ['./subject.component.scss']
+})
+export class SubjectComponent implements OnInit {
+
+  subjects: Subject[] | undefined;
+
+  constructor(private subjectService: SubjectService,) { }
+
+  ngOnInit(): void {
+    this.getAll();
+  }
+
+  public getAll(): void {
+    this.subjectService.getAll().subscribe((subjects) => {
+      this.subjects = subjects;
+    })
+  }
+
+}
