df7884eb98be30c11a330d472943209b8c21fd8c
Frederic Guyont <you@example.com>
2024-04-24 13:01:58 +0200

    ADD: subject list page style

--- front/src/app/pages/subject/subject.component.ts
+++ front/src/app/pages/subject/subject.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Subject } from 'src/app/models/subject';
+import { User } from 'src/app/models/user';
 import { SubjectService } from 'src/app/services/subject.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-subject',
@@ -10,11 +12,20 @@ import { SubjectService } from 'src/app/services/subject.service';
 export class SubjectComponent implements OnInit {
 
   subjects: Subject[] | undefined;
+  public user: User | undefined;
 
-  constructor(private subjectService: SubjectService,) { }
+  constructor(private subjectService: SubjectService,private userService: UserService,) { }
 
   ngOnInit(): void {
+    this.getUser();
     this.getAll();
+  }
+
+  public getUser() {
+    this.userService
+      // .getById(this.sessionService.sessionInformation!.id.toString())
+      .getConnectedUser()
+      .subscribe((user: User) => this.user = user );
   }
 
   public getAll(): void {
@@ -25,7 +36,16 @@ export class SubjectComponent implements OnInit {
 
   public follow(id: number) {
     this.subjectService.follow(id.toString()).subscribe(() => {
+      this.getUser();
+      this.getAll();
     });
+  }
+
+  public isAlreadyFollowed(subjectId: number): boolean{
+    if (this.user) {
+    return this.user?.subjectIds.includes(subjectId);
+    }
+    return false;
   }
 
 }
