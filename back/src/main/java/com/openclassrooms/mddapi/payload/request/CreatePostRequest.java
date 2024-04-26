7189fb2b89def513c3ea496472741c9e1337f8d3
Frederic Guyont <you@example.com>
2024-04-26 12:52:43 +0200

    FIX: models and dtos annotations

--- back/src/main/java/com/openclassrooms/mddapi/payload/request/CreatePostRequest.java
+++ back/src/main/java/com/openclassrooms/mddapi/payload/request/CreatePostRequest.java
@@ -2,16 +2,16 @@ package com.openclassrooms.mddapi.payload.request;
 
 import lombok.Data;
 
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Data
 public class CreatePostRequest {
-    @NotBlank
+    @NotNull
     @Size(max = 50)
     private String title;
 
-    @NotBlank
+    @NotNull
     @Size(max = 5000)
     private String text;
 
