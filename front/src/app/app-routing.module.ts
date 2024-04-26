5c4c23a4fce10e1cc0ce14ce8c21e6034d525a8d
Frederic Guyont <you@example.com>
2024-04-26 13:03:55 +0200

    ADD: guards

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
@@ -8,17 +8,17 @@ import { SubjectComponent } from './pages/subject/subject.component';
 import { UserComponent } from './pages/user/user.component';
 import { CreatePostComponent } from './pages/create-post/create-post.component';
 import { PostDetailComponent } from './pages/post-detail/post-detail.component';
+import { UnauthGuard } from './guards/unauth.guard';
+import { AuthGuard } from './guards/auth.guard';
 
-// consider a guard combined with canLoad / canActivate route option
-// to manage unauthenticated user to access private routes
 const routes: Routes = [{ path: '', component: HomeComponent },
-{ path: 'login', component: LoginComponent },
-{ path: 'register', component: RegisterComponent },
-{ path: 'post', component: PostComponent },
-{ path: 'subject', component: SubjectComponent },
-{ path: 'user', component: UserComponent},
-{ path: 'createpost', component: CreatePostComponent},
-{ path: 'post/:id', component: PostDetailComponent}
+{ path: 'login', component: LoginComponent, canActivate: [UnauthGuard] },
+{ path: 'register', component: RegisterComponent, canActivate: [UnauthGuard] },
+{ path: 'post', component: PostComponent, canActivate: [AuthGuard] },
+{ path: 'subject', component: SubjectComponent, canActivate: [AuthGuard] },
+{ path: 'user', component: UserComponent, canActivate: [AuthGuard] },
+{ path: 'createpost', component: CreatePostComponent, canActivate: [AuthGuard] },
+{ path: 'post/:id', component: PostDetailComponent, canActivate: [AuthGuard] }
 
 ];
 
